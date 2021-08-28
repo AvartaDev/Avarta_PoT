a1ac9863f901ac0e483a3d5b7c8da093a1e4f4e1
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-08-28 04:29:22 +0400

    implement network

---
+++ src/libs/networkConstants.js
@@ -0,0 +1,7 @@
+export const MAINNET = 'mainnet';
+export const ROPSTEN = 'ropsten';
+export const KOVAN = 'kovan';
+export const RINKEBY = 'rinkeby';
+export const BSC = 'BSC';
+export const BSC_TEST = 'BSC_TEST';
+export const SOLANA_TEST = 'SOLANA_TEST';
